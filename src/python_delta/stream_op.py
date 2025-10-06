2646523f3ef5972d6233495979b840c1ccd87469
Joseph Cutler <jwc@seas.upenn.edu>
2025-10-06 10:54:29 -0400

    over to DONE-based iterators

--- src/python_delta/stream_op.py
+++ src/python_delta/stream_op.py
@@ -2,6 +2,23 @@ from python_delta.event import CatEvA, CatPunc, ParEvA, ParEvB, PlusPuncA, PlusP
 from enum import Enum
 
 
+class Done:
+    """Sentinel value indicating a stream is exhausted."""
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __repr__(self):
+        return "Done"
+
+
+# Singleton instance
+DONE = Done()
+
+
 class CatRState(Enum):
     """State machine for CatR operation."""
     FIRST_STREAM = 0   # Pulling from first stream (wrapped in CatEvA)
@@ -31,9 +48,21 @@ class StreamOp:
         """Make StreamOp iterable."""
         return self
 
+    def _pull(self):
+        """Pull the next element from the stream.
+
+        Returns:
+            - A value (including None for skips)
+            - DONE sentinel when stream is exhausted
+        """
+        raise NotImplementedError("Subclasses must implement _pull")
+
     def __next__(self):
         """Pull the next element from the stream. Raise StopIteration when exhausted."""
-        raise NotImplementedError("Subclasses must implement __next__")
+        result = self._pull()
+        if result is DONE:
+            raise StopIteration
+        return result
 
     def reset(self):
         """Reset the stream to its initial state for reuse."""
@@ -57,11 +86,14 @@ class Var(StreamOp):
     def __str__(self):
         return f"Var({self.name}: {self.stream_type})"
 
-    def __next__(self):
+    def _pull(self):
         """Pull from the source iterator."""
         if self.source is None:
             raise RuntimeError(f"Var '{self.name}' has no source bound")
-        return next(self.source)
+        try:
+            return next(self.source)
+        except StopIteration:
+            return DONE
 
     def reset(self):
         """Var has no internal state to reset."""
@@ -84,9 +116,9 @@ class Eps(StreamOp):
     def __str__(self):
         return f"Eps({self.stream_type})"
 
-    def __next__(self):
-        """Always raise StopIteration - empty stream has no elements."""
-        raise StopIteration
+    def _pull(self):
+        """Always return DONE - empty stream has no elements."""
+        return DONE
 
     def reset(self):
         """Eps has no internal state to reset."""
@@ -107,24 +139,23 @@ class CatR(StreamOp):
     def vars(self):
         return self.input_streams[0].vars | self.input_streams[1].vars
 
-    def __next__(self):
+    def _pull(self):
         """Pull from first stream (wrapped in CatEvA), then CatPunc, then second stream (unwrapped)."""
         if self.current_state == CatRState.FIRST_STREAM:
-            try:
-                val = next(self.input_streams[0])
-                if val is None:
-                    return None
-                return CatEvA(val)
-            except StopIteration:
+            val = self.input_streams[0]._pull()
+            if val is DONE:
                 self.current_state = CatRState.EMIT_PUNC
                 return CatPunc()
+            if val is None:
+                return None
+            return CatEvA(val)
         elif self.current_state == CatRState.EMIT_PUNC:
             self.current_state = CatRState.SECOND_STREAM
-            val = next(self.input_streams[1])
-            return val  # Unwrapped (including None skips)
+            val = self.input_streams[1]._pull()
+            return val  # Unwrapped (including None skips and DONE)
         else:  # CatRState.SECOND_STREAM
-            val = next(self.input_streams[1])
-            return val  # Unwrapped (including None skips)
+            val = self.input_streams[1]._pull()
+            return val  # Unwrapped (including None skips and DONE)
 
     def reset(self):
         """Reset state and recursively reset input streams."""
@@ -154,16 +185,15 @@ class CatProjCoordinator(StreamOp):
         For position 2: skips CatEvA events until CatPunc is seen, then returns unwrapped tail events
         """
         if self.input_exhausted:
-            raise StopIteration
+            return DONE
 
         if position == 1 and self.seen_punc:
-            raise StopIteration
+            return DONE
 
-        try:
-            event = next(self.input_stream)
-        except StopIteration:
+        event = self.input_stream._pull()
+        if event is DONE:
             self.input_exhausted = True
-            raise
+            return DONE
 
         if position == 1:
             # Position 1: return CatEvA values, stop at CatPunc
@@ -171,7 +201,7 @@ class CatProjCoordinator(StreamOp):
                 return event.value
             elif isinstance(event, CatPunc):
                 self.seen_punc = True
-                raise StopIteration
+                return DONE
             elif event is None:
                 return None
             else:
@@ -190,7 +220,7 @@ class CatProjCoordinator(StreamOp):
                 # After CatPunc, return unwrapped tail events
                 return event
 
-    def __next__(self):
+    def _pull(self):
         """Coordinators are not directly iterable."""
         raise NotImplementedError("CatProjCoordinator should not be iterated directly")
 
@@ -218,7 +248,7 @@ class CatProj(StreamOp):
     def __str__(self):
         return f"CatProj{self.position}({self.stream_type})"
 
-    def __next__(self):
+    def _pull(self):
         return self.coordinator.pull_for_position(self.position)
 
     def reset(self):
@@ -242,12 +272,12 @@ class SumInj(StreamOp):
     def vars(self):
         return self.input_stream.vars
 
-    def __next__(self):
+    def _pull(self):
         """Emit tag first (PlusPuncA if position=0, PlusPuncB if position=1), then pull from input stream."""
         if not self.tag_emitted:
             self.tag_emitted = True
             return PlusPuncA() if self.position == 0 else PlusPuncB()
-        return next(self.input_stream)
+        return self.input_stream._pull()
 
     def reset(self):
         """Reset state and recursively reset input stream."""
@@ -260,58 +290,44 @@ class CaseOp(StreamOp):
     def __init__(self, input_stream, left_branch, right_branch, stream_type):
         super().__init__(stream_type)
         self.input_stream = input_stream
-        self.left_branch = left_branch  # StreamOp that produces output
-        self.right_branch = right_branch  # StreamOp that produces output
-        # self.left_var = left_var  # Var node in left branch
-        # self.right_var = right_var  # Var node in right branch
-        self.active_branch = None  # Will be set to left_branch or right_branch after reading tag
+        self.branches = [left_branch,right_branch] # StreamOp that produces output
+        self.active_branch = -1
         self.tag_read = False
 
     @property
     def id(self):
-        return hash(("CaseOp", self.input_stream.id, self.left_branch.id, self.right_branch.id))
+        return hash(("CaseOp", self.input_stream.id, self.branches[0].id, self.branches[1].id))
 
     @property
     def vars(self):
-        return self.input_stream.vars | self.left_branch.vars | self.right_branch.vars
+        return self.input_stream.vars | self.branches[0].vars | self.branches[1].vars
 
-    def __next__(self):
+    def _pull(self):
         """Read tag and route to appropriate branch."""
         if not self.tag_read:
-            tag = next(self.input_stream)
+            tag = self.input_stream._pull()
             if tag is None:
                 return None
+            if tag is DONE:
+                return DONE
             self.tag_read = True
 
             if isinstance(tag, PlusPuncA):
-                self.active_branch = self.left_branch
+                self.active_branch = 0
             elif isinstance(tag, PlusPuncB):
-                self.active_branch = self.right_branch
+                self.active_branch = 1
             else:
                 raise RuntimeError(f"Expected PlusPuncA or PlusPuncB tag, got {tag}")
             return None
 
-        return next(self.active_branch)
+        if self.active_branch == -1:
+            raise RuntimeError("CaseOp._pull() called before tag was read")
+        return self.branches[self.active_branch]._pull()
 
     def reset(self):
         """Reset state and recursively reset branches."""
         self.tag_read = False
         self.active_branch = None
-
-# class MapOp(StreamOp):
-#     def __init__(self,subgraph,input,stream_type):
-#         super().__init__(stream_type)
-#         self.subgraph = subgraph
-#         self.input = input
-
-#     def __next__(self):
-        # first we pull from the input.
-        # (1) if it's a pluspuncA, we send pluspuncA and stop!
-        # (2) if it's a pluspuncB, we send pluspuncB, and move to "mapping" mode.
-        # in mapping mode,
-        # pull from the subgraph's output. the subgraph's input is a catevA-peel of the input.
-        # when this raises, we send catpunc, and reset the subgraph's state
-
 
 class SinkThen(StreamOp):
     """Sink operation - pulls from first stream until exhausted, then switches to second stream."""
@@ -328,20 +344,20 @@ class SinkThen(StreamOp):
     def vars(self):
         return self.input_streams[0].vars | self.input_streams[1].vars
 
-    def __next__(self):
+    def _pull(self):
         """Pull from first stream until exhausted, then switch to second stream."""
         if not self.first_exhausted:
-            try:
-                # Pull from first stream and drop the value (sink it)
-                next(self.input_streams[0])
-                return None  # Drop all values from first stream
-            except StopIteration:
+            # Pull from first stream and drop the value (sink it)
+            val = self.input_streams[0]._pull()
+            if val is DONE:
                 # First stream exhausted, switch to second
                 self.first_exhausted = True
                 # Fall through to pull from second stream
+            else:
+                return None  # Drop all values from first stream
 
         # Pull from second stream
-        return next(self.input_streams[1])
+        return self.input_streams[1]._pull()
 
     def reset(self):
         """Reset state."""
@@ -362,9 +378,10 @@ class ResetOp(StreamOp):
     def vars(self):
         return set()
 
-    def __next__(self):
+    def _pull(self):
         for node in self.reset_set:
             node.reset()
+        return None
 
     def reset(self):
         pass
@@ -383,9 +400,9 @@ class UnsafeCast(StreamOp):
     def vars(self):
         return self.input_stream.vars
 
-    def __next__(self):
+    def _pull(self):
         """Forward data from input stream without modification."""
-        return next(self.input_stream)
+        return self.input_stream._pull()
 
     def reset(self):
         pass
