ea180ff14bc11754ca0b29199492b5fe031b2c8e
Joseph Cutler <jwc@seas.upenn.edu>
2025-10-30 12:48:04 -0400

    splitz endz

--- tests/test_splitz.py
+++ tests/test_splitz.py
@@ -88,66 +88,29 @@ def test_splitz_cons_onez():
     ]
     assert result == expected_result
 
+def test_splitz_cons_endz():
+    @Delta.jit
+    def f(delta,s : TyStar(INT_TY)):
+        return delta.splitZ(s)
 
-# def test_concatmap_nil_cons():
-#     @Delta.jit
-#     def f(delta,s : TyStar(INT_TY)):
-#         return delta.concat_map(s,lambda _ : delta.nil())
-
-#     output = f(iter([PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(4)),CatPunc(),PlusPuncA()]))
-#     result = [x for x in list(output) if x is not None]
-#     assert result[0] == PlusPuncA()
-
-# def test_concatmap_id_nil():
-#     @Delta.jit
-#     def f(delta,s : TyStar(INT_TY)):
-#         return delta.concat_map(s,lambda x : delta.cons(x,delta.nil()))
-
-#     output = f(iter([PlusPuncA()]))
-#     result = [x for x in list(output) if x is not None]
-#     assert result[0] == PlusPuncA()
-
-# def test_concatmap_inj():
-#     @Delta.jit
-#     def f(delta,s : TyStar(INT_TY)):
-#         return delta.concat_map(s,lambda x : delta.cons(x,delta.nil()))
-
-#     input = [PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(4)),CatPunc(),PlusPuncA()]
-#     output = f(iter(input))
-#     result = [x for x in list(output) if x is not None]
-#     assert result == input
-
-
-# def test_concatmap_one():
-#     @Delta.jit
-#     def f(delta,s : TyStar(INT_TY)):
-#         return delta.concat_map(s,lambda x : delta.cons(delta.singleton(1),delta.cons(x,delta.nil())))
-
-#     input = [PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncA()]
-#     output = f(iter(input))
-#     result = [x for x in list(output) if x is not None]
-#     assert result == [PlusPuncB(),CatEvA(BaseEvent(1)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncA()]
-
-# def test_concatmap_one_another():
-#     @Delta.jit
-#     def f(delta,s : TyStar(INT_TY)):
-#         return delta.concat_map(s,lambda x : delta.cons(delta.singleton(1),delta.cons(x,delta.nil())))
-
-#     input = [PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(4)),CatPunc(),PlusPuncA()]
-#     output = f(iter(input))
-#     result = [x for x in list(output) if x is not None]
-#     assert result == [PlusPuncB(),CatEvA(BaseEvent(1)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(1)),CatPunc(),PlusPuncB(),CatEvA(BaseEvent(4)),CatPunc(),PlusPuncA()]
-
-
-# @given(events_of_type(TyStar(INT_TY), max_depth=5))
-# def test_map_proj1_preserves_types(input_events):
-#     input_type = TyStar(INT_TY)
-#     @Delta.jit
-#     def f(delta,s : input_type):
-#         return delta.concat_map(s,lambda x : delta.cons(delta.singleton(1),delta.cons(x,delta.nil())))
-
-#     assert has_type(input_events,input_type)
-#     output = f(iter(input_events))
-#     result = [x for x in list(output) if x is not None]
-
-#     assert has_type(result, input_type), f"Output does not have type {input_type}"
+    input = [PlusPuncB(),CatEvA(BaseEvent(1)),CatPunc(),
+             PlusPuncB(),CatEvA(BaseEvent(2)),CatPunc(),
+             PlusPuncB(),CatEvA(BaseEvent(3)),CatPunc(),
+             PlusPuncB(),CatEvA(BaseEvent(5)),CatPunc(),
+             PlusPuncB(),CatEvA(BaseEvent(6)),CatPunc(),
+             PlusPuncB(),CatEvA(BaseEvent(0)),CatPunc(),
+             PlusPuncA()
+             ]
+    output = f(iter(input))
+    result = [x for x in list(output) if x is not None]
+    expected_result = [
+        CatEvA(PlusPuncB()),CatEvA(CatEvA(BaseEvent(1))),CatEvA(CatPunc()),
+        CatEvA(PlusPuncB()),CatEvA(CatEvA(BaseEvent(2))),CatEvA(CatPunc()),
+        CatEvA(PlusPuncB()),CatEvA(CatEvA(BaseEvent(3))),CatEvA(CatPunc()),
+        CatEvA(PlusPuncB()),CatEvA(CatEvA(BaseEvent(5))),CatEvA(CatPunc()),
+        CatEvA(PlusPuncB()),CatEvA(CatEvA(BaseEvent(6))),CatEvA(CatPunc()),
+        CatEvA(PlusPuncA()),
+        CatPunc(),
+        PlusPuncA()
+    ]
+    assert result == expected_result
